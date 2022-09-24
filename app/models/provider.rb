77a4b96344cc9735672601b4b2bb59a3ef67b1bf
Juan Contreras <jpcontreras.dev@gmail.com>
2022-09-24 07:10:40 -0500

    feat(provider.rb provider_spec.rb): unit tests created to provider model

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,7 +1,7 @@
 class Provider < ApplicationRecord
   validates :name, presence: true
   validates :nit, format: { with: /\A([0-9]{9,})-([0-9]{1,})\z/, message: 'invalid format' }
-  validates :contact_name
+  validates :contact_name, presence: true
   validates :contact_cellphone, :length => { :maximum => 10 }
   validates :account_number, :length => { :maximum => 15 }
 
