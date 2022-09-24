a6db8204cd3550ea5863840f4ff98c1104ed1ab1
Juan Contreras <jpcontreras.dev@gmail.com>
2022-09-24 12:25:14 -0500

    feat(provider_serializer.rb es.yml): validation created to return default value when any attribute is blank in the index action of providers controller

--- app/serializers/provider_serializer.rb
+++ app/serializers/provider_serializer.rb
@@ -1,7 +1,20 @@
 class ProviderSerializer < ActiveModel::Serializer
   attributes :id, :name, :contact_name, :contact_cellphone, :bank_name, :account_number
 
+  def contact_cellphone
+    return self.is_field_blank?(object.contact_cellphone)
+  end
+
+  def account_number
+    return self.is_field_blank?(object.account_number)
+  end
+
   def bank_name
     object.bank.name if object.bank.present?
+  end
+
+  private
+  def is_field_blank?(field)
+    return I18n.t('messages.attributes.provider.blank') if field.blank?
   end
 end
